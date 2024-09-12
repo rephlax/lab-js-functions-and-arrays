79fa1de9e9e6d1f6e04cb302bf46a121b0d55a1c
Luke Farrell <lukefarrell2012@gmail.com>
2024-09-12 19:23:21 +0100

    Lab work complete Week3 Day2 task 1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,13 +1,35 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
+// function maxOfTwoNumbers() {}
+const maxOfTwoNumbers = (num1, num2) => {
+    if (num1 === num2) {
+        return num1, num2;
+    } else if (num1 > num2){
+        return num1;
+    } else if (num1 < num2){
+        return num2;
+    } else {
+        console.error("Cannot compare");
+    }
+}
 
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+// function findLongestWord() {}
+    const findLongestWord = (words) => {
+        let longestWord = "";
+        if (words.length === 0) {
+            return null;
+        } else {
+            words.forEach(word => {
+                if (longestWord.length < word.length){
+                    longestWord = word;
+                }
+            })
+        } return longestWord;
+    }
 
 
 
@@ -15,7 +37,12 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+// function sumNumbers() {}
+    const sumNumbers = (numbers) => {
+        initialValue = 0;
+        const sum = numbers.reduce((total, currentNumber) => total + currentNumber, initialValue);
+        return sum;
+    }
 
 
 
@@ -23,12 +50,25 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
+// function averageNumbers() {}
+    const averageNumbers = (numbers2) => {
+        initialValue = 0;
+        if (numbers2.length === 0) {
+            return 0;
+        }
+        const sum2 = numbers2.reduce((total, currentNumber) => total + currentNumber, initialValue);
+        return sum2 / numbers2.length;
+    }
 
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+// function doesWordExist() {}
+    const doesWordExist = (array, word) => {
+        if (array.length === 0 ){
+            return null
+        }
+        return (array.includes(word));
+    }
